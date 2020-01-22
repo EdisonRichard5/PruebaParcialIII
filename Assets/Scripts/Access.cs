df491ed595b8b5c88760ac2f10676a0ee2948414
smperez1997 <52365447+smperez1997@users.noreply.github.com>
2020-01-22 08:26:13 -0500

    script

---
+++ Assets/Scripts/Access.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Access : MonoBehaviour
+{
+    public GameObject player;
+    public string URIDataBase;
+    public Button boton;
+    public GameObject input;
+    public GameObject input1;
+    private DBConnector _connector;
+   
+   
+
+        public void guardar()
+    {
+        _connector = gameObject.AddComponent<DBConnector>();
+        _connector.OpenDB("URI=file:Assets\\db\\" + URIDataBase);
+        _connector.InsertData(input.GetComponent<Text>().text, player.transform.position.x.ToString(), player.transform.position.y.ToString(), player.transform.position.z.ToString());
+        _connector.CloseDB();
+      
+    }
+    public void Start()
+    {
+
+        boton.onClick.AddListener(guardar);
+      
+    }
+
+
+}
---
+++ Assets/Scripts/DBConnector.cs
@@ -0,0 +1,62 @@
+﻿ using Mono.Data.Sqlite;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DBConnector : MonoBehaviour
+{
+    private SqliteConnection _conexion;
+    private SqliteCommand _command;
+    private SqliteDataReader _reader;
+
+    private string _query;
+    public void OpenDB(string _dbName)
+    {
+        _conexion = new SqliteConnection(_dbName);
+        _conexion.Open();
+    }
+
+    public void SelectData()
+    {
+        _query = "SELECT * FROM Person";
+        _command.CommandText = _query;
+        _reader = _command.ExecuteReader();
+        if (_reader != null)
+        {
+            while (_reader.Read())
+            {
+                print(_reader.GetValue(1).ToString() + "-" + _reader.GetValue(2).ToString());
+            }
+        }
+    }
+
+
+    
+    public void InsertData(string _puntos, string _x, string _y, string _z)
+{
+
+    _query = "INSERT INTO Person VALUES(2,'" + _puntos + "','" + _x + "','" + _y + "','" + _z + "')";
+    _command = _conexion.CreateCommand();
+    _command.CommandText = _query;
+    _command.ExecuteReader();
+       
+}
+
+public void UpdateData(string _name)
+{
+    _query = " UPDATE Person SET Name = '" + _name + "' WHERE id =1";
+    _command = _conexion.CreateCommand();
+    _command.CommandText = _query;
+    _command.ExecuteReader();
+}
+
+public void CloseDB()
+{
+    _reader.Close();
+    _reader = null;
+    _command = null;
+    _conexion.Close();
+    _conexion = null;
+}
+
+}
